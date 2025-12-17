b3349d53091e6a319604a0371bb41502d954a066
Linda <bengtsson-linda@outlook.com>
2025-12-17 11:41:57 +1100

    remove duplicated files

--- src/lib/harvest/projectBudgets 2.ts
+++
@@ -1,9 +0,0 @@
-import { harvestFetch } from "./apiClient";
-
-/*
- Fetches project budget reports from Harvest API
- */
-export async function getProjectBudget() {
-  const data = await harvestFetch("/reports/project_budget");
-  return data;
-}
